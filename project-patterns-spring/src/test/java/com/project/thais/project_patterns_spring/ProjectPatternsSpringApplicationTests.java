f607c10a28d2f03c418bcb664c8159f0ec2daa17
Thais-Abe <72356503+Thais-Abe@users.noreply.github.com>
2024-07-16 16:13:40 -0300

    add initials files

---
+++ project-patterns-spring/src/test/java/com/project/thais/project_patterns_spring/ProjectPatternsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.thais.project_patterns_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPatternsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
